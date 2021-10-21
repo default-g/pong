26d599072ccd6769dfbc6f7ff7bd19bb4553ca83
Artem <arteam_101@mail.ru>
2021-10-21 23:25:23 +0700

    Started making in OOP way

--- main.cpp
+++ main.cpp
@@ -11,132 +11,206 @@ float rand_float(float a, float b) {
 
 using namespace sf;
 
-class Object{
+class Object {
 
-  protected:
+protected:
+  sf::Color color;
+  sf::Vector2f position;
+  sf::Drawable *shape;
 
-    sf::Color color;
-    sf::Vector2f position;
-    sf::Shape *shape;
+public:
+  Object() : position(sf::Vector2f(0, 0)), color(sf::Color::White) {
+    this->shape = new sf::RectangleShape(sf::Vector2f(10, 10));
+    ((sf::Shape *)(this->shape))->setFillColor(color);
+    ((sf::Shape *)(this->shape))->setPosition(position);
+  };
 
-  public:
+  Object(sf::Drawable *shape) : shape(shape) {
+    position = ((sf::Shape *)(this->shape))->getPosition();
+    color = ((sf::Shape *)(this->shape))->getFillColor();
+  };
 
-    Object(): position(sf::Vector2f(0, 0)), color(sf::Color::White) {
-      this->shape = new sf::RectangleShape(sf::Vector2f(10, 10));
-      shape->setFillColor(color);
-      shape->setPosition(position);
-    };
+  Object(float x, float y, sf::Color color = sf::Color::White,
+         sf::Drawable *shape = new sf::RectangleShape(sf::Vector2f(10, 10))) {
+    this->position = sf::Vector2f(x, y);
+    this->color = color;
+    this->shape = shape;
+    ((sf::Shape *)(this->shape))->setFillColor(color);
+    ((sf::Shape *)(this->shape))->setPosition(position);
+  };
 
-    Object(sf::Shape *shape) : shape(shape){
-      position = shape->getPosition();
-      color = shape->getFillColor();
-    };
+  Object(const Object &obj) {
+    this->shape = obj.shape;
+    this->color = obj.color;
+    this->position = obj.position;
+  }
 
-    Object(float x, float y, sf::Color color = sf::Color::White, sf::Shape *shape = new sf::RectangleShape(sf::Vector2f(10, 10))) {
-      this->position = sf::Vector2f(x, y);
-      this->color = color;
-      this->shape = shape;
-      this->shape->setFillColor(color);
-      this->shape->setPosition(position);
-    };
+  const sf::Drawable *get_shape() { return shape; };
 
-    Object(const Object& obj) {
-      this->shape = obj.shape;
-      this->color = obj.color;
-      this->position = obj.position;
-    }
+  sf::Color get_color() { return ((sf::Shape *)shape)->getFillColor(); };
 
-    const sf::Shape *get_shape(){
-      return shape;
-    };
+  sf::Vector2f get_position() { return ((sf::Shape *)shape)->getPosition(); };
 
-    sf::Color get_color(){
-      return shape->getFillColor();
-    };
-
-    sf::Vector2f get_position(){
-      return shape->getPosition();
-    };
-
+  void set_position(sf::Vector2f position){ 
+    this->position = position;
+    ((sf::Shape *)shape)->setPosition(position);}
 };
 
-class MovableObject: public Object {
+class ScoreText : public Object {
 
-  protected:
-    MovableObject(float x, float y, sf::Color color = sf::Color::White, sf::Shape *shape = new sf::RectangleShape(sf::Vector2f(10, 10))): Object(x, y, color, shape){
-      start_position = position;
-    };
-    virtual void move() = 0;
-    virtual void update_position() = 0;
+private:
+  std::string value;
+  sf::Font font;
+  int character_size;
 
-    float speed;
-    sf::Vector2f start_position;
-    sf::Vector2f direction;
+public:
+  ScoreText(int value, float x, float y, std::string font_path = "font.ttf",
+            int character_size = 30)
+      : value(std::to_string(value)), character_size(character_size) {
+    this->shape = new sf::Text;
+    this->font.loadFromFile(font_path);
+    this->color = color;
+    this->position = sf::Vector2f(x, y);
+    ((sf::Text *)(this->shape))->setPosition(position);
+    ((sf::Text *)(this->shape))->setCharacterSize(this->character_size);
+    ((sf::Text *)(this->shape))->setFont(this->font);
+    ((sf::Text *)(this->shape))->setFillColor(this->color);
+    ((sf::Text *)(this->shape))->setString(this->value);
+  };
 
-    sf::Vector2f get_direction() {
-      return direction;
-    };
+  void set_value(std::string value) {
+    this->value = value;
+    ((sf::Text *)(this->shape))->setString(this->value);
+  };
 
-    float get_speed() {
-      return speed;
-    };
+  void set_value(int value) {
+    this->value = std::to_string(value);
+    ((sf::Text *)(this->shape))->setString(this->value);
+  };
 
-    sf::Vector2f get_start_position(){
-      return start_position;
-    }   
+  std::string get_value() { return this->value; };
 };
 
-class newPlayer: public MovableObject {
-  private:
+class MovableObject : public Object {
 
-  int score = 0;
+protected:
+  MovableObject(float x, float y, sf::Color color = sf::Color::White,
+                sf::Drawable *shape = new sf::RectangleShape(sf::Vector2f(10,
+                                                                          10)))
+      : Object(x, y, color, shape) {
+    start_position = position;
+  };
+  virtual void move() = 0;
+  virtual void update_position() = 0;
+
+  float speed;
+  sf::Vector2f start_position;
+  sf::Vector2f direction;
+public:
+  sf::Vector2f set_direction(float x, float y) {
+    this->direction = sf::Vector2f(x, y);
+  };
+
+  sf::Vector2f get_direction() { return direction; };
+
+  float get_speed() { return speed; };
+
+  sf::Vector2f get_start_position() { return start_position; };
+
+  void reset_position() { this->position = this->start_position; }
+};
+
+class newPlayer : public MovableObject {
+private:
+  int score;
 
   void move() { return; };
 
-  public:
-
-  newPlayer(float x, float y, float speed, sf::Color color = sf::Color::White):  MovableObject(x, y, color, new RectangleShape(sf::Vector2f(20, 100))) {
+public:
+  newPlayer(float x, float y, float speed, sf::Color color = sf::Color::White)
+      : MovableObject(x, y, color, new RectangleShape(sf::Vector2f(20, 100))) {
     this->speed = speed;
+    this->score = 0;
   };
 
-    void move_down() {
-      this->position.y += this->speed;
-    };
+  void move_down() { this->position.y += this->speed; };
 
-    void move_up() {
-      this->position.y -= this->speed;
-    };
+  void move_up() { this->position.y -= this->speed; };
 
-    void update_position() {
-      if (this->position.y < 0)
+  void update_position() {
+    if (this->position.y < 0)
       this->position.y = 0;
 
-      if (this->position.y > 600)
+    if (this->position.y > 600)
       this->position.y = 600;
-      this->shape->setPosition(this->position);
-    };
+    ((sf::Shape *)(this->shape))->setPosition(this->position);
+  };
 
-    void reset_position(){
-      this->position = this->start_position;
-    }
+  int get_score() { return this->score; };
 
-    int get_score(){
-      return this->score;
-    };
-
-    void plus_score(){
-      this->score++;
-    };
-
+  void plus_score() { this->score++; };
 };
 
-class newBall: public MovableObject{
+class newBall : public MovableObject {
+private:
+  void update_position(){};
+public:
+  newBall(float x, float y, float speed, sf::Color color = sf::Color::Red)
+      : MovableObject(x, y, color, new CircleShape(15)){
+        this->speed = speed;
+        float angle = rand_float(0, 360 * PI / 180);
+        direction = sf::Vector2f(sin(angle), cos(angle));
+      };
 
-  public:
+    void move(){
+      position = sf::Vector2f(position.x + direction.x * speed, position.y + direction.y * speed);
+    };
 
-    newBall(float x, float y, sf::Color color = sf::Color::Red): MovableObject(x, y, color, new CircleShape(15)) {};
+    void update_position(newPlayer &player1, newPlayer &player2){
+      if (position.y < 0) {
+      direction.y *= -1;
+    }
+    if (position.y > 670) {
+      direction.y *= -1;
+    }
+    if (((sf::CircleShape*)(this->get_shape()))->getGlobalBounds().intersects(
+            ((sf::Shape*)(player1.get_shape()))->getGlobalBounds())) {
+      float new_angle = rand_float(-45 * PI / 180, 45 * PI / 180);
+      direction.x = cos(new_angle);
+      direction.y = sin(new_angle);
+      position.x += 0.1;
+    }
+   if (((sf::CircleShape*)(this->get_shape()))->getGlobalBounds().intersects(
+            ((sf::Shape*)(player2.get_shape()))->getGlobalBounds())) {
+      float new_angle = rand_float(135 * PI / 180, 225 * PI / 180);
+      direction.x = cos(new_angle);
+      direction.y = sin(new_angle);
 
-
+      position.x -= 0.1;
+    }
+    if (position.x <= 25) {
+      player1.reset_position();
+      player2.reset_position();
+      float new_angle = rand_float(-45 * PI / 180, 45 * PI / 180);
+      direction.x = cos(new_angle);
+      direction.y = sin(new_angle);
+      position.x = 500;
+      position.y = 350;
+      player2.plus_score();
+    }
+    if (position.x >= 950) {
+      player1.reset_position();
+      player2.reset_position();
+      float new_angle = rand_float(135 * PI / 180, 225 * PI / 180);
+      direction.x = cos(new_angle);
+      direction.y = sin(new_angle);
+      position.x = 500;
+      position.y = 350;
+      player1.plus_score();
+    }
+    this->set_position(position);
+    }
+  
 };
 
 class Player {
@@ -248,34 +322,29 @@ int main() {
   music.play();
   music.setLoop(true);
   sf::Font font;
-  font.loadFromFile("font.ttf");
-  sf::Text score1(std::to_string(player1.score), font);
-  sf::Text score2(std::to_string(player2.score), font);
-  score1.setCharacterSize(30);
-  score2.setCharacterSize(30);
-  score1.setColor(sf::Color::White);
-  score2.setColor(sf::Color::White);
-  score1.setPosition(250, 20);
-  score2.setPosition(750, 20);
+  ScoreText scores[] = {ScoreText(player1.score, 250, 20),
+                        ScoreText(player2.score, 750, 20)};
+
   sf::RenderWindow window(sf::VideoMode(1000, 700), "Pong");
   window.setVerticalSyncEnabled(true);
   window.setFramerateLimit(60);
 
+  Object game_field_elements[] = {
+      Object(0, 0, sf::Color::White,
+             new sf::RectangleShape(sf::Vector2f(10, 700))),
+      Object(500, 0, sf::Color::White,
+             new sf::RectangleShape(sf::Vector2f(10, 700))),
+      Object(990, 0, sf::Color::White,
+             new sf::RectangleShape(sf::Vector2f(10, 700)))};
 
-  Object gameFieldElements[] {
-    Object(0, 0, sf::Color::White, new sf::RectangleShape(sf::Vector2f(10, 700))),
-    Object(500, 0, sf::Color::White, new sf::RectangleShape(sf::Vector2f(10, 700))),
-    Object(990, 0, sf::Color::White, new sf::RectangleShape(sf::Vector2f(10, 700)))
-  };
-  
   player1.update_position();
   player2.update_position();
 
   Ball ball = Ball(500, 350);
 
   while (window.isOpen()) {
-    score1.setString(std::to_string(player1.score));
-    score2.setString(std::to_string(player2.score));
+    scores[0].set_value(player1.score);
+    scores[1].set_value(player2.score);
     sf::Event event;
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
@@ -309,12 +378,13 @@ int main() {
 
     window.draw(player1.get_shape());
     window.draw(player2.get_shape());
-    for(int i = 0; i < 3; i++){
-      window.draw(*gameFieldElements[i].get_shape());
+    for (int i = 0; i < 3; i++) {
+      window.draw(*game_field_elements[i].get_shape());
     };
+    for (int i = 0; i < 2; i++) {
+      window.draw(*scores[i].get_shape());
+    }
     window.draw(ball.get_shape());
-    window.draw(score1);
-    window.draw(score2);
 
     window.draw(*plr.get_shape());
 
